2d3e282d2bbad44cda1f27ce3caa43ad51277434
morteza.m <morteza.m@MORTEZAM-PC>
2018-09-01 15:36:06 +0430

    Change ghasedak incoming message to latest doc.

--- FanapPlus.Ghasedak.Client/FanapPlus.Ghasedak.Client/Models/Incoming/GhasedakIncomingMessage.cs
+++ FanapPlus.Ghasedak.Client/FanapPlus.Ghasedak.Client/Models/Incoming/GhasedakIncomingMessage.cs
@@ -1,13 +1,19 @@
-﻿namespace FanapPlus.Ghasedak.Client.Models.Incoming
+﻿using System;
+
+namespace FanapPlus.Ghasedak.Client.Models.Incoming
 {
     public class GhasedakIncomingMessage
     {
         public string Muid { get; set; }
-        public string AccountId { get; set; }
-        public string Status { get; set; }
         public string Sid { get; set; }
+        public DateTime ReceiveTime { get; set; }
         public ChannelType ChannelType { get; set; }
-        public string Reason { get; set; }
-        public string Permanent { get; set; }
+        public string Channel { get; set; }
+        public string Actor { get; set; }
+        public string AccountId { get; set; }
+        public string UserPhoneNumber { get; set; }
+        public MessageType MessageType { get; set; }
+        public string Content { get; set; }
+        public string Signature { get; set; }
     }
 }
