3fe8a9d632811cee1d1d5a3ed97918c31f1a2e24
morteza.m <morteza.m@MORTEZAM-PC>
2018-09-02 13:58:11 +0430

    Add default Uid and Date for GhasedakOutgoingMessageRequest.

--- FanapPlus.Ghasedak.Client/FanapPlus.Ghasedak.Client/Models/Outgoing/GhasedakOutgoingMessageRequest.cs
+++ FanapPlus.Ghasedak.Client/FanapPlus.Ghasedak.Client/Models/Outgoing/GhasedakOutgoingMessageRequest.cs
@@ -6,6 +6,11 @@ namespace FanapPlus.Ghasedak.Client.Models.Outgoing
 {
     public class GhasedakOutgoingMessageRequest
     {
+        public GhasedakOutgoingMessageRequest()
+        {
+            Uid = Guid.NewGuid().ToString("N");
+            Date = DateTime.UtcNow;
+        }
         public string Uid { get; set; }
         public DateTime Date { get; set; }
         public IEnumerable<GhasedakContentMessage> Messages { get; set; }
