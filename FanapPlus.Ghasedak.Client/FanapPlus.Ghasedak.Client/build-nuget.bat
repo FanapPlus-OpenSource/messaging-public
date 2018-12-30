d725a7cc51ef5374562bdd1025cb2e2b52903845
morteza.m <morteza.m@MORTEZAM-PC>
2018-12-30 16:43:38 +0330

    Change nuget spec.

--- FanapPlus.Ghasedak.Client/FanapPlus.Ghasedak.Client/build-nuget.bat
+++ FanapPlus.Ghasedak.Client/FanapPlus.Ghasedak.Client/build-nuget.bat
@@ -1,1 +1,1 @@
-dotnet pack -c Release -p:PackageVersion=1.1.8 /p:NuspecFile=FanapPlus.Ghasedak.Client.nuspec
+dotnet pack -c Release -p:PackageVersion=1.1.9 /p:NuspecFile=FanapPlus.Ghasedak.Client.nuspec
