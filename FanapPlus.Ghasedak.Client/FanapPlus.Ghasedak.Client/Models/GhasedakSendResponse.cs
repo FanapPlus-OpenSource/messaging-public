c8ca42fd48df8e9be5b1b49f365cb4477a26e952
morteza.m <morteza.m@MORTEZAM-PC>
2018-12-30 09:55:02 +0330

    Add SyncDeliveryStatuses.

--- FanapPlus.Ghasedak.Client/FanapPlus.Ghasedak.Client/Models/GhasedakSendResponse.cs
+++ FanapPlus.Ghasedak.Client/FanapPlus.Ghasedak.Client/Models/GhasedakSendResponse.cs
@@ -6,5 +6,6 @@ namespace FanapPlus.Ghasedak.Client.Models
     {
         public string Puid { get; set; }
         public IEnumerable<string> Muids { get; set; }
+        public IEnumerable<string> SyncDeliveryStatuses { get; set; }
     }
 }
