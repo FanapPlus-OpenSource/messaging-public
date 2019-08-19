288b6dfcd203a48f470220d16e8e651a473693f2
Hoda Sedighi <sedighi.iut@gmail.com>
2019-08-19 10:01:58 +0430

    add details proplerty to change response model

--- FanapPlus.Ghasedak.Client/FanapPlus.Ghasedak.Client/Models/GhasedakSendResponse.cs
+++ FanapPlus.Ghasedak.Client/FanapPlus.Ghasedak.Client/Models/GhasedakSendResponse.cs
@@ -7,5 +7,6 @@ namespace FanapPlus.Ghasedak.Client.Models
         public string Puid { get; set; }
         public IEnumerable<string> Muids { get; set; }
         public IEnumerable<string> SyncDeliveryStatuses { get; set; }
+        public IEnumerable<ResponseDeatil> ResponseDeatils { get; set; }
     }
 }
---
+++ FanapPlus.Ghasedak.Client/FanapPlus.Ghasedak.Client/Models/ResponseDeatil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FanapPlus.Ghasedak.Client.Models
+{
+    public class ResponseDeatil
+    {
+        public string Status { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+}
