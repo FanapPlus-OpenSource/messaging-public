11ff7ff48d8c0b8aadfe7cf2099acbeb90844427
morteza.m <morteza.m@MORTEZAM-PC>
2019-09-24 14:51:08 +0330

    update build script.

--- FanapPlus.Ghasedak.Client/FanapPlus.Ghasedak.Client/build-nuget.bat
+++ FanapPlus.Ghasedak.Client/FanapPlus.Ghasedak.Client/build-nuget.bat
@@ -1,2 +1,2 @@
 dotnet build -c Release
-dotnet pack -c Release -p:PackageVersion=2.0.3 /p:NuspecFile=FanapPlus.Ghasedak.Client.nuspec
+dotnet pack -c Release /p:NuspecFile=FanapPlus.Ghasedak.Client.nuspec
